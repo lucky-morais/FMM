75f84d53acc3a8815495f9daeee3583181a91303
Paulo <lucky_morais@hotmail.com>
2019-07-12 18:50:38 +0100

    Limpeza de bugs #1

--- Cliente.java
+++ Cliente.java
@@ -1,17 +1,28 @@
-public class Cliente {
+import java.lang.Math;
 
-  public boolean toma(Cafe cafe) throws CafeQuenteException,
+public class Cliente {
+    private String nome;
+    private int quente;
+    private int frio;
+    
+    public Cliente(String nome, int quente, int frio) {
+        this.nome = nome;
+        this.quente = quente;
+        this.frio = frio;
+    }
+
+    public boolean toma(Cafe cafe) throws CafeQuenteException,
                                         CafeFrioException,
                                         CafeException {
-    int decisao = 1 + (int) (random()*100);
+    int decisao = 1 + (int) (Math.random()*100);
     if(decisao < 10) 
       throw new CafeException("Mal tirado!");
     
     int temp = cafe.getTemp();
     if(temp > quente)
-      throw new CafeQuenteException("Quente + " (temp-quente));
+      throw new CafeQuenteException("Quente + " + (temp - quente));
     if(temp < frio)
-      throw new CafeQuenteException("Frio - " (temp-frio));      
+      throw new CafeQuenteException("Frio - " + (frio - temp));      
       
     return true;
   }
--- TstCafeBar.java
+++ TstCafeBar.java
@@ -3,11 +3,19 @@ import static java.lang.System.out;
 public class TstCafeBar {
   public static void main(String[] args) {
     Cliente cliente1 = new Cliente("Rui", 60, 75);
-    Cafe cafe = new Cafe(67);
+    Cliente cliente2 = new Cliente("Ana", 65, 77);
+    Cliente cliente3 = new Cliente("João", 66, 80);    
+    Cafe cafe1 = new Cafe(80);
+    Cafe cafe2 = new Cafe(64);
+    Cafe cafe3 = new Cafe(78);
+    
     try {
-      cliente1.tomaCafe(cafe);
-    } catch(CafeException ce) { out.println(ce.getMessage());   }
-      catch(CafeQuenteException ce) { out.println(ce.getMessage());   }
-       catch(CafeFrioException ce) { out.println(ce.getMessage());   }
+      out.print("A servir café ao cliente #1 : "); cliente1.toma(cafe1);
+      out.print("A servir café ao cliente #2 : "); cliente2.toma(cafe2);
+      out.print("A servir café ao cliente #3 : "); cliente3.toma(cafe3);      
+    } 
+      catch(CafeQuenteException ece2) { out.println(ece2.getMessage());   }
+       catch(CafeFrioException ece3) { out.println(ece3.getMessage());   }
+       catch(CafeException ece1) { out.println(ece1.getMessage());   }
   }
 }
